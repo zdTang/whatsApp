9b8f79862192737dfbecb2335a019b428aa2273b
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-06 16:52:32 -0400

    2  add a button

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
+const clickHandler = () => console.log("Hi Mike");
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi gus!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="clickme"
+        // onPress={() => {
+        //   console.log("Hi Mike");
+        // }}
+        onPress={clickHandler}
+      />
     </View>
   );
 }
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
