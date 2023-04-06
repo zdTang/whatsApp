7104cd6421ae3b358da29293f673cb09ba4b6c57
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-06 17:13:32 -0400

    3. Safe Area
    using Safe Area package to avoid display content on un-safe area

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const clickHandler = () => console.log("Hi Mike");
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hi gus!</Text>
-      <StatusBar style="auto" />
-      <Button
-        title="clickme"
-        // onPress={() => {
-        //   console.log("Hi Mike");
-        // }}
-        onPress={clickHandler}
-      />
-    </View>
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView>
+        <Text>Hi gus!</Text>
+        <StatusBar style="auto" />
+        <Button
+          title="clickme"
+          // onPress={() => {
+          //   console.log("Hi Mike");
+          // }}
+          onPress={clickHandler}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -22,7 +25,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    //alignItems: "center",
+    //justifyContent: "center",
   },
 });
